5c9c61ae88f618e21b1189d0ccdabdf9907576e2
Geraldoeze <egerald344@gmail.com>
2022-02-09 23:40:08 +0100

    working on mvc

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express()
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
@@ -15,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', { pageTitle: 'Error Page'})
 });
 
 app.listen(3500);
