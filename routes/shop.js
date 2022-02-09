329f7808bc3b2a34854a74f0d4c531f62f235d0c
Geraldoeze <egerald344@gmail.com>
2022-02-09 18:55:39 +0100

    working with ejs

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path')
+
 const routes = express.Router();
+const adminData = require('./admin');
 
 routes.get('/', (req, res, next )=> {
-    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+    console.log(adminData.products)
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 })
 
 module.exports = routes;
