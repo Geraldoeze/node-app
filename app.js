c47e7bd397364b553de35e36d7b240627cabd599
Geraldoeze <egerald344@gmail.com>
2022-02-09 23:55:48 +0100

    using routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express()
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.set('view engine', 'ejs');
@@ -13,7 +13,7 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
