0854049cbcab25fec89959b10a4fe331575446ea
Geraldoeze <egerald344@gmail.com>
2022-02-10 22:24:37 +0100

    added more views

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const bodyParser = require('body-parser');
 const path = require('path');
 
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')))
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.errorPage);
+app.use(errorController.get404);
 
-app.listen(3500);  
+app.listen(3000);
--- controllers/error.js
+++ controllers/error.js
@@ -1,5 +1,3 @@
-exports.errorPage = (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Error Page',
-            path: '/'
-    })
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
