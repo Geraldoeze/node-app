4f1f2c6e6dbb34479fa99b8e943f6bb8063688be
Geraldoeze <egerald344@gmail.com>
2022-02-11 00:49:26 +0100

    added more css styles

--- controllers/shop.js
+++ controllers/shop.js
@@ -27,10 +27,10 @@ exports.getCart = (req, res, next) => {
   });
 };
 
-exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
   });
 };
 
