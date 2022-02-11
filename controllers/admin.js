0f39c5144f075d4c63add554f768454f4998f93d
Geraldoeze <egerald344@gmail.com>
2022-02-11 22:59:17 +0100

    setTimeout

--- controllers/admin.js
+++ controllers/admin.js
@@ -3,7 +3,8 @@ const Product = require('../models/product');
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product'
+    path: '/admin/add-product',
+    editing: false
   });
 };
 
@@ -12,17 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageURL = req.body.imageURL;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageURL, description, price);
+  const product = new Product(null, title, imageURL, description, price);
   product.save();
   res.redirect('/');
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-  //If edit is not added  to  URL param this will run
-  //  if (!editMode) {
-  //    return res.redirect('/');
-  //  }
+  // If edit is not added  to  URL param this will run
+   if (!editMode) {
+     return res.redirect('/');
+   }
   const prodId = req.params.productId;
   Product.findById(prodId, product => {
     if(!product) {
@@ -38,6 +39,23 @@ exports.getEditProduct = (req, res, next) => {
  
 };
 
+exports.postEditProducts = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedPrice = req.body.price;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDesc,
+    updatedPrice
+    );
+    updatedProduct.save();
+    res.redirect('/admin/products')
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
@@ -47,3 +65,10 @@ exports.getProducts = (req, res, next) => {
     });
   });
 };
+ 
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId);
+  res.redirect('/admin/products')
+}
--- models/product.js
+++ models/product.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const pathHelper = require('../util/path');
-
+const Cart = require('./cart');
 
 const p = path.join(
     pathHelper,
@@ -21,23 +21,47 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-    constructor(title, imageURL, description, price) {
+    constructor(id, title, imageURL, description, price) {
+        this.id = id;
         this.title = title;
         this.imageURL = imageURL;
         this.description = description;
         this.price = price;
     }
 
-    save() {
-        this.id = Math.random().toString();
+    save() {        
         getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            })
+            if (this.id) {
+                const existingProductIndex = products.findIndex(prod => prod.id === this.id);
+                const updatedProducts = [...products];
+                updatedProducts[existingProductIndex] = this;
+                fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+                    console.log(err);
+                });    
+            } else {
+                this.id = Math.random().toString();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), (err) => {
+                    console.log(err);
+                })
+            }
+        
         });
         
     }
+
+    static deleteById(id) {
+        getProductsFromFile(products => {
+            const product = products.find(prod => prod.id === id);
+            const updatedProduct = products.filter(prod => prod.id !== id);
+            fs.writeFile(p, JSON.stringify(updatedProduct), err => {
+                if (!err) {
+                    Cart.deleteProduct(id, product.price)
+                }
+            })
+        })
+    }
+
 
     static fetchAll( cb ) {
         getProductsFromFile(cb)
