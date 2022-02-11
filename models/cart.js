defc9c87fa56e30ce3e0aa1f72e57dc0c7d78de3
Geraldoeze <egerald344@gmail.com>
2022-02-11 12:04:03 +0100

    added cart logic

--- models/cart.js
+++ models/cart.js
@@ -12,31 +12,36 @@ const p = path.join(
 
 module.exports = class Cart {
     static addProduct(id, productPrice) {
+        console.log(productPrice)
         //Fetch the previous cart
         fs.readFile(p, (err, fileContent) => {
             let cart = {products: [], totalPrice: 0 };
             if (!err) {
                 cart = JSON.parse(fileContent);
-            }
+            } 
             //Analyze the cart => Find existing product
             const existingProductIndex = cart.products.findIndex(prod => prod.id === id);
             const existingProduct = cart.products[existingProductIndex]
             let updatedProduct ;
+
+            //Add new product/ increase quantity
+
             if (existingProduct) {
                 updatedProduct = {...existingProduct};
                 updatedProduct.qty = updatedProduct.qty + 1;
                 cart.products = [...cart.products];
                 cart.products[existingProductIndex] = updatedProduct
-            } else {
+           }
+         else {
                 updatedProduct = { id: id, qty: 1};
                  cart.products = [...cart.products, updatedProduct]
-            }
-            cart.totalPrice = cart.totalPrice + productPrice;
-           fs.watchFile(p, JSON.stringify(cart), (err) =>{
+            } 
+            cart.totalPrice = cart.totalPrice + +productPrice;
+            console.log(cart)
+           fs.writeFile(p, JSON.stringify(cart), (err) =>{
                console.log(err);
            })
         });
-        //Add new product/ increase quantity
     }
 
 }
