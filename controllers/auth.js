698b300b77ce58f1254d4b85dec64640778a8e60
Geraldoeze <egerald344@gmail.com>
2022-02-28 23:41:44 +0100

    added resetToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,8 +9,8 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   auth: {
-    user: 'geraldoeze201@gmail.com',
-    pass: '09053357654'
+    user: 'd.gallant650@gmail.com',
+    pass: 'D.gallant701'
   },
 });
 
@@ -164,3 +164,55 @@ exports.postReset = (req, res, next) => {
       })
   });
 }
+
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({resetToken: token, resetTokenExp: {$gt: Date.now()}})
+  .then(user => {
+    let message = req.flash('error');
+      if (message) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render('auth/new-password', {
+        path: '/new-passord',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        passwordToken: token, 
+        userId: user._id.toString()
+      })
+  })
+  .catch(err => console.log(err))
+  
+}
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExp: {$gt: Date.now() },
+    _id: userId
+  })
+  .then(user => {
+    resetUser = user;
+    return bcrypt.hash(newPassword, 12);
+  })
+  .then(hashedPassword => {
+    resetUser.password = hashedPassword;
+    resetUser.resetToken = undefined;
+    resetUser.resetTokenExp = undefined;
+    return resetUser.save();
+  } )
+  .then(result => {
+    res.redirect('/login');
+  })
+  .catch(err => {
+    console.log(err);
+  })
+};
