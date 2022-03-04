a2fdb0f6bbf3c73c88d91351ad77119d5bf92cb0
Geraldoeze <egerald344@gmail.com>
2022-03-04 18:37:38 +0100

    error handled

--- controllers/admin.js
+++ controllers/admin.js
@@ -65,7 +65,9 @@ exports.postAddProduct = (req, res, next) => {
       //   errorMessage: errors.array()[0].msg,
       //   validationErrors: []
       //});      
-      res.redirect('/500');
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
 
     });
 };
@@ -91,7 +93,11 @@ exports.getEditProduct = (req, res, next) => {
         validationErrors: []
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postEditProduct = (req, res, next) => {
@@ -135,7 +141,11 @@ exports.postEditProduct = (req, res, next) => {
         res.redirect('/admin/products');
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.getProducts = (req, res, next) => {
@@ -162,5 +172,9 @@ exports.postDeleteProduct = (req, res, next) => {
       console.log('DESTROYED PRODUCT');
       res.redirect('/admin/products');
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
