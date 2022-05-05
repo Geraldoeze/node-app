9601e37e6465dd7913e708d18aa42b678d350e8c
Gerald Ezenagu <egerald344@gmail.com>
2022-05-05 23:40:10 +0100

    removed pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -25,28 +25,14 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const page = +req.query.page || 1;
-  let totalItems;
-  
+
   Product.find()
-   .countDocuments()
-   .then(numProducts => {
-     totalItems = numProducts;
-     return Product.find()
-      .skip((page - 1) * ITEMS_PER_PAGE)
-      .limit(ITEMS_PER_PAGE);
-  }).then(products => {
+  .then(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'Products',
       path: '/products',
-      currentPage: page,  
-      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-      hasPrevPage: page > 1,
-      nextPage: page + 1,
-      previousPage: page - 1,
-      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
-    });
+      });
   })
     .catch(err => {
       const error = new Error(err);
@@ -56,28 +42,14 @@ exports.getProducts = (req, res, next) => {
 }; 
 
 exports.getIndex = (req, res, next) => {
-  const page = +req.query.page || 1;
-  let totalItems;
-  
+
   Product.find()
-   .countDocuments()
-   .then(numProducts => {
-     totalItems = numProducts;
-     return Product.find()
-      .skip((page - 1) * ITEMS_PER_PAGE)
-      .limit(ITEMS_PER_PAGE);
-  }).then(products => {
+  .then(products => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
       path: '/',
-      currentPage: page,  
-      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-      hasPrevPage: page > 1,
-      nextPage: page + 1,
-      previousPage: page - 1,
-      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
-    });
+      });
   })
     .catch(err => {
     console.log(err)
